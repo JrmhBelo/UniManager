198a6814b67ad5ba4abbe39ccd0e0f36050d61ae
João Belo <jrmhbelo@gmail.com>
2020-11-19 18:49:35 +0000

    Tut2e3Commit1

---
+++ src/main/java/pt/iade/unimanage/models/Student.java
@@ -0,0 +1,84 @@
+package pt.iade.unimanage.models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class Student {
+    private static int nextNumber = 0;
+    private String name;
+    private LocalDate birthDate;
+    private String email;
+    private char gender;
+    private int number;
+    private ArrayList<Unit> units;
+    private ArrayList<Enrolment> enrolments;
+
+    public Student(String name, LocalDate birthDate,char gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.number = nextNumber;
+        nextNumber++;
+        email = "";
+        units = new ArrayList<Unit>();
+        enrolments = new ArrayList<Enrolment>();
+    }
+
+    public static int getNextNumber() { 
+        return nextNumber; 
+    }
+
+    public String getName() {
+         return name; 
+    }
+
+    public void setName(String name) {
+         this.name = name; 
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate date) {
+        this.birthDate = date;
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    public char getGender() {
+        return gender; 
+    }
+
+    public void setGender(char gender){
+        this.gender = gender;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public ArrayList<Unit> getUnits() {
+        return units;
+    }
+
+    public ArrayList<Enrolment> getEnrolments() {
+        return enrolments;
+    }
+    
+	public Enrolment getEnrolmentByUnitId(int unitId) {
+		return null;
+	}
+    
+}
